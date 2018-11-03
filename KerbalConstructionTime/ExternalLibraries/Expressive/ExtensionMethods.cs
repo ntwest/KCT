136a184d9a4905ca8b9df3a7636140c9df901bb3
Nate West <westnt@outlook.com>
2018-11-02 21:29:38 -0400

    Add Expressive Library (MIT license) for use to replace formula parser later.

---
+++ KerbalConstructionTime/ExternalLibraries/Expressive/ExtensionMethods.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Expressive
+{
+    internal static class ExtensionMethods
+    {
+        internal static bool IsArithmeticOperator(this string source)
+        {
+            return string.Equals(source, "+", StringComparison.Ordinal) ||
+                string.Equals(source, "-", StringComparison.Ordinal) || string.Equals(source, "\u2212", StringComparison.Ordinal) ||
+                string.Equals(source, "/", StringComparison.Ordinal) || string.Equals(source, "\u00f7", StringComparison.Ordinal) ||
+                string.Equals(source, "*", StringComparison.Ordinal) || string.Equals(source, "\u00d7", StringComparison.Ordinal) ||
+                string.Equals(source, "+", StringComparison.Ordinal) ||
+                string.Equals(source, "+", StringComparison.Ordinal) ||
+                string.Equals(source, "+", StringComparison.Ordinal);
+        }
+
+        internal static bool IsNumeric(this string source)
+        {
+            double test;
+            return double.TryParse(source, out test);
+        }
+
+        internal static T PeekOrDefault<T>(this Queue<T> queue)
+        {
+            return (queue.Count > 0) ? queue.Peek() : default(T);
+        }
+
+        internal static string SubstringUpTo(this string source, int startIndex, char character)
+        {
+            if (startIndex != 0)
+            {
+                string inner = source.Substring(startIndex);
+
+                return inner.Substring(0, inner.IndexOf(character) + 1);
+            }
+
+            return source.Substring(startIndex, source.IndexOf(character) + 1);
+        }
+
+        internal static bool IsNullOrWhiteSpace(String source)
+        {
+            if (source == null) return true;
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (!Char.IsWhiteSpace( source[i] )) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ KerbalConstructionTime/ExternalLibraries/Expressive/Helpers/Comparison.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Expressive.Helpers
+{
+    internal static class Comparison
+    {
+        private static readonly Type[] CommonTypes = new[] { typeof(long), typeof(int), typeof(double), typeof(bool), typeof(DateTime), typeof(string), typeof(decimal) };
+
+        internal static int CompareUsingMostPreciseType(object a, object b, bool ignoreCase)
+        {
+            Type mostPreciseType = GetMostPreciseType(a.GetType(), b.GetType());
+
+            if (mostPreciseType == typeof(string))
+            {
+                return string.Compare((string)Convert.ChangeType(a, mostPreciseType), (string)Convert.ChangeType(b, mostPreciseType), ignoreCase);
+            }
+
+            return Comparison.Compare(a, b, mostPreciseType, ignoreCase);
+        }
+
+        internal static Type GetMostPreciseType(Type a, Type b)
+        {
+            foreach (Type t in Comparison.CommonTypes)
+            {
+                if (a == t || b == t)
+                {
+                    return t;
+                }
+            }
+
+            return a;
+        }
+
+        private static int Compare(object lhs, object rhs, Type mostPreciseType, bool ignoreCase)
+        {
+            // If at least one is null then the check is simple.
+            if (lhs == null && rhs == null)
+            {
+                return 0;
+            }
+            else if (lhs == null)
+            {
+                return -1;
+            }
+            else if (rhs == null)
+            {
+                return 1;
+            }
+
+            var lhsType = lhs.GetType();
+            var rhsType = rhs.GetType();
+
+            if (lhsType == rhsType)
+            {
+                return Comparer<object>.Default.Compare(lhs, rhs);
+            }
+            else
+            {
+                // Attempt to convert using the mostPreciseType first.
+                try
+                {
+                    if (lhsType == mostPreciseType)
+                    {
+                        rhs = Convert.ChangeType(rhs, mostPreciseType);
+                    }
+                    else
+                    {
+                        lhs = Convert.ChangeType(lhs, mostPreciseType);
+                    }
+
+                    return Comparer<object>.Default.Compare(lhs, rhs);
+                }
+                catch (System.Exception)
+                {
+
+                }
+
+                // Attempt to convert the RHS to match the LHS.
+                try
+                {
+                    return Comparer<object>.Default.Compare(lhs, Convert.ChangeType(rhs, lhsType));
+                }
+                catch (System.Exception)
+                {
+
+                }
+
+                // Attempt to convert the LHS to match the RHS.
+                try
+                {
+                    return Comparer<object>.Default.Compare(lhs, Convert.ChangeType(rhs, lhsType));
+                }
+                catch (System.Exception)
+                {
+
+                }
+
+                // Failing that resort to a string.
+                try
+                {
+                    return string.Compare((string)Convert.ChangeType(lhs, typeof(string)),
+                                          (string)Convert.ChangeType(rhs, typeof(string)),
+                                          ignoreCase);
+                }
+                catch (System.Exception)
+                {
+
+                }
+            }
+
+            return 0;
+        }
+    }
+}
